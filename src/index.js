5793e1bd323c7be7d8148a98ef3afe4e1cd6f18e
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-09-03 12:49:35 -0300

    feat: add user service, controller e routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Rodando!");
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta: ${port}`);
 });
